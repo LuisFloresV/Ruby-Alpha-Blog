bdcc355cc4509ffc5fed35f33dc37e6e2d9405eb
Luis Flores V <65628587+LuisFloresV@users.noreply.github.com>
2020-10-26 11:13:45 -0600

    User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+    VALID_EMAIL_REGEX = URI::MailTo::EMAIL_REGEXP
+    
+    validates :username, presence: true, 
+                        uniqueness: { case_sensitive: false },
+                        length: {minimum: 3, maximum: 25}
+                       
+
+    validates :email, presence: true, 
+                        uniqueness: { case_sensitive: false }, 
+                        length: {maximum: 105}, 
+                        format: {with: VALID_EMAIL_REGEX}
+
 end
