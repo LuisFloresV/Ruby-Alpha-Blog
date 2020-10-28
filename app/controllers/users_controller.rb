1bb3c26998327989e38fba32cbd278f7f0868dc0
Luis Flores V <65628587+LuisFloresV@users.noreply.github.com>
2020-10-27 22:56:45 -0600

    Navigation changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
     def new
         @user = User.new
     end
@@ -13,25 +14,35 @@ class UsersController < ApplicationController
         end
     end
 
-    def edit
-        @user = User.find(params[:id])
+    def edit 
     end
 
     def update
-        @user = User.find(params[:id])
         if @user.update(user_params)
             flash[:notice] = "Your user has been updated!"
-            redirect_to articles_path
+            redirect_to @user
         else
             render 'edit'
         end
-
-
     end
+    
+    def show
+        @articles = @user.articles
+    end
+
+    def index
+        @users = User.all
+    end
+
+
 
 
     private
    
+    def set_user
+        @user = User.find(params[:id])
+    end
+
 
     def user_params
         params.required(:user).permit(:username, :email, :password)
